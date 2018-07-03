da9fa65becb34caf86be62b0304296a44532bd94
ma0603kang@163.com <maxiaokang0603>
2018-07-03 13:55:02 +0800

    修改接口名称

--- financeManager/src/main/java/com/hxl/structrue/action/TestServlet.java
+++ financeManager/src/main/java/com/hxl/structrue/action/TestServlet.java
@@ -13,9 +13,12 @@ public class TestServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println("已经进入到Servlet中");
+        Long startTime = System.currentTimeMillis();
         TestConn testConn =  (TestConn)SpringHelper.getObject("demoService");
         String ret_str = testConn.testHear("传入数据");
+        Long endTime = System.currentTimeMillis();
         System.out.println("服务方返回数据::"+ret_str);
+        System.out.println("总计耗时:"+(endTime-startTime));
 
     }
 }
