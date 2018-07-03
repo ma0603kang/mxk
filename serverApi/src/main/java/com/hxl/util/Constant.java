99fa3e77cfa6e8020044b3b946bcec33f4901556
ma0603kang@163.com <maxiaokang0603>
2018-07-03 13:20:43 +0800

    修改接口名称

--- serverApi/src/main/java/com/hxl/util/Constant.java
+++ serverApi/src/main/java/com/hxl/util/Constant.java
@@ -2,13 +2,14 @@ package com.hxl.util;
 
 /**
 *@Title:
-*@Description:定义静态常亮类
+*@Description:定义静态常量类
 *@Author:Rose
 *@Since:2018年3月19日
 *@Version:1.1.0
 */
     
 public class Constant {
+
 	// content
 	public static final String PATH = "path";
 	//数据Key
