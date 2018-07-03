9293c2639571053ef341a3b7d699e1f1c0d2dc2e
ma0603kang@163.com <maxiaokang0603>
2018-07-03 13:34:38 +0800

    修改接口名称

--- financeManager/src/main/java/com/hxl/structrue/action/TestServlet.java
+++ financeManager/src/main/java/com/hxl/structrue/action/TestServlet.java
@@ -12,7 +12,7 @@ public class TestServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        System.out.println("已经进入到Servlet中123");
+        System.out.println("已经进入到Servlet中");
         TestConn testConn =  (TestConn)SpringHelper.getObject("demoService");
         String ret_str = testConn.testHear("传入数据");
         System.out.println("服务方返回数据::"+ret_str);
